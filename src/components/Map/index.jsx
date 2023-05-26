73511485878b637ed87a12050ba4df81b2a4a67c
Andy Willis <awillis@fastmail.fm>
2023-05-26 01:23:54 +0100

    Added map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,13 +1,34 @@
+import { useEffect } from 'preact/hooks';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 import style from './style.module.css';
 
 function Map() {
+
+  useEffect(() => {
+
+    const home = [ 51.275710, 1.336495 ];
+
+    const map = L.map('mapid', { dragging: !L.Browser.mobile }).setView(home, 12);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+      minZoom: 3,
+      bounds: [
+        [ -180, 180 ],
+        [ 180, -180 ]
+      ]
+    }).addTo(map);
+
+  }, []);
+
   return (
-    <section class={style.mapContainer}>
-      <header>
-        <h4>Map</h4>
-      </header>
-    </section>
+    <section class={style.mapContainer} id="mapid" />
   );
+
 }
 
 export default Map;
