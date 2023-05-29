64dbba12274ed2c9f174741e063b7ead1b8a533e
Andy Willis <awillis@fastmail.fm>
2023-05-29 01:23:04 +0100

    Restructured

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -3,7 +3,9 @@ import { effect } from '@preact/signals';
 
 import * as L from 'leaflet';
 
-import { geoData, currentWalk, darkMode } from '../../store';
+// import { geoData, currentWalk, darkMode } from '../../store';
+
+import updateMap from '../../effects/updateMap';
 
 import 'leaflet/dist/leaflet.css';
 import style from './style.module.css';
@@ -24,8 +26,7 @@ function Map() {
     mapRef.current = L.map('mapid', { dragging: !L.Browser.mobile }).setView(home, 12);
 
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       maxZoom: 18,
       minZoom: 3,
       bounds: [
@@ -35,48 +36,7 @@ function Map() {
     }).addTo(mapRef.current);
 
     // Signals effect
-    effect(() => {
-
-      const walk = geoData.value.find(obj => {
-        return obj.id === currentWalk.value;
-      });
-
-      const route = L.geoJSON(walk.route, {
-        style: {
-          ...walk.style.route,
-          ...style.line
-        }
-      });
-
-      const markers = walk.markers.features.map(marker => {
-
-        const {
-          geometry: { coordinates },
-          properties: { icons: { svg } }
-        } = marker;
-
-        const [ lng, lat ] = coordinates;
-
-        return L.marker([ lat, lng ], {
-          icon: L.divIcon({
-            html: svg.html[darkMode.value ? 'dark' : 'light'],
-            // iconUrl: `../../assets/images/icons/${png.name}.png`,
-            // iconSize: [ 5, 5 ],
-            // iconAnchor: [ 16, 37 ]
-          })
-        });
-
-      });
-
-      L.featureGroup([ route, ...markers ]).addTo(mapRef.current);
-
-      // walk.markers.forEach(marker => {
-      //   L.geoJSON(walk.markers).addTo(mapRef.current);
-      // });
-
-      mapRef.current.fitBounds(route.getBounds());
-
-    });
+    effect(() => updateMap(mapRef));
 
   }, []);
 
