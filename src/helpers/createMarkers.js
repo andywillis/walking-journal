48215f34114465c68b96e97d56887b4910132b70
Andy Willis <awillis@fastmail.fm>
2023-06-08 11:32:45 +0100

    Marker accessibility

--- src/helpers/createMarkers.js
+++ src/helpers/createMarkers.js
@@ -20,20 +20,26 @@ function createMarkers(data, darkMode) {
 	
 		const [ lng, lat ] = coordinates;
 	
-		const popupHtml = `
-			<b>${name}</b><br/><br/>
-			${description}<br/><br/>
-			<section style="display: flex; align-items: center;">
-				<img style="margin-right: 0.25rem;" title="www.geograph.org.uk icon" src="${geoGraphIcon}" /><a target="_blank" style="font-size: 0.8rem;" href="https://www.geograph.org.uk/browse.php?q=${osGridRef}">${osGridRef}</a></a>
-			</section>
-		`;
-
 		const marker = L.marker([ lat, lng ], {
 			icon: L.divIcon({
 				html: icons[darkMode ? 'dark' : 'light'],
 				className: ''
-			})
-		}).bindPopup(popupHtml);
+			}),
+			title: name,
+			alt: `${name} - ${description}`
+		}, {});
+
+		if (description) {
+			const popupHtml = `
+				<b>${name}</b><br/><br/>
+				${description}<br/><br/>
+				<section style="display: flex; align-items: center;">
+					<img style="margin-right: 0.25rem;" title="www.geograph.org.uk icon" src="${geoGraphIcon}" />
+					<a target="_blank" style="font-size: 0.8rem;" href="https://www.geograph.org.uk/browse.php?q=${osGridRef}">${osGridRef}</a></a>
+				</section>
+			`;
+			marker.bindPopup(popupHtml);
+		}
 	
 		return marker;
 
